01db52befecd07d9a7e786d5e36eef2804c3c1a3
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-09-30 14:29:23 +0000

    Work on utils.c

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,17 +3,18 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-    /* To be implemented by the students */
-    printf("%s is not yet implemented!\n", __func__);
+    if (lsb == NULL)
+        return 1;
 
-    return 1;
+    *lsb = val;
+    return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-    /* To be implemented by the students */
-    printf("%s is not yet implemented!\n", __func__);
-
-    return 1;
+    if (*msb == NULL)
+        return 1;
+    *msb = val >> 8;
+    return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
