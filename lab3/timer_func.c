69f0613a7356213c9e5fb05526ac242c6264ccfb
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-10-15 14:05:21 +0000

    minor change

---
+++ lab3/timer_func.c
@@ -0,0 +1,15 @@
+#include "timer_func.h"
+
+#include <lcom/lcf.h>
+#include "i8254.h"
+
+int (subscribe_timer_interrupt)(uint8_t interrupt_bit, int *interrupt_id) {
+    if (interrupt_id == NULL) return 1;
+    *interrupt_id = interrupt_bit;
+    return (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, interrupt_id));
+}
+
+int no_interrupts = 0;
+void (timer_int_handler)() {
+    no_interrupts++;
+}
