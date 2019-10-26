db052a56a946c7c0bf1773f3f39e0e83102d4862
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-10-26 22:43:02 +0000

    utils.h on lab2

--- lab2/utils.c
+++ lab2/utils.c
@@ -14,10 +14,17 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
     return 0;
 }
 
+#ifdef LAB3
+    uint32_t sys_inb_counter = 0;
+#endif
+
 int (util_sys_inb)(int port, uint8_t *value) {
     if(value == NULL) return 1;
     uint32_t n = 0;
     if(sys_inb(port, &n)) return 1;
     *value = n;
+    #ifdef LAB3
+        ++sys_inb_counter;
+    #endif
     return 0;
 }
