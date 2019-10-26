7d0bd161e4a3329b93b09f7b00fc733d6086f4d8
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-10-26 19:44:01 +0000

    added code to mouse_test_packet

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -31,7 +31,48 @@ int main(int argc, char *argv[]) {
 
 
 int (mouse_test_packet)(uint32_t cnt) {
-    return 1;
+    /// loop stuff
+    int ipc_status, r;
+    message msg;
+    /// Keyboard interrupt handling
+    uint8_t mouse_irq_bit = 12;
+    int mouse_id = 0;
+    int mouse_irq = BIT(mouse_irq_bit);
+    if (subscribe_mouse_interrupt(mouse_irq_bit, &mouse_id)) return 1;
+    if(mouse_enable_data_reporting()) return 1;
+    /// cycle
+    int good = 1;
+    while (good) {
+        /* Get a request message. */
+        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+            printf("driver_receive failed with %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE: /* hardware interrupt notification */
+                    if (msg.m_notify.interrupts & mouse_irq) { /* subscribed interrupt */
+                        mouse_ih();
+                        /*
+                        if (!(two_byte_scancode || got_error)) { // finished processing a scancode
+                            if (scancode[0] == TWO_BYTE_CODE) kbd_print_scancode(!(scancode[1] & BREAK_CODE_BIT), 2, scancode);
+                            else                              kbd_print_scancode(!(scancode[0] & BREAK_CODE_BIT), 1, scancode);
+                        } else { break; }
+                        if (scancode[0] == ESC_BREAK_CODE) good = 0;
+                        */
+                    }
+                    break;
+                default:
+                    break; /* no other notifications expected: do nothing */
+            }
+        } else { /* received standart message, not a notification */
+            /* no standart message expected: do nothing */
+        }
+    }
+
+    if (unsubscribe_interrupt(&mouse_id)) return 1;
+
+    return 0;
 }
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
