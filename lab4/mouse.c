ce260e134a9ab1b09f965b92079954adc5822589
up201806554 <up201806554@84c98234-7846-4868-9730-29c3f42930e9>
2019-11-04 18:24:42 +0000

    fixed stuff

--- lab4/mouse.c
+++ lab4/mouse.c
@@ -91,7 +91,7 @@ int (mouse_issue_cmd)(uint32_t cmd) {
     for (unsigned int i = 0; i < KBC_NUM_TRIES; i++) {
         if ((ret = kbc_issue_cmd(MOUSE_WRITE_B))) return ret;
         if ((ret = kbc_issue_arg(cmd))) return ret;
-        if ((ret = mouse_read_ack(&ack))) return ret;
+        if ((ret = mouse_read_byte(&ack))) return ret;
 
         if (ack == ACK_OK) return SUCCESS;
         if (ack == ACK_ERROR) return INVALID_COMMAND;
@@ -113,21 +113,6 @@ int (mouse_read_byte)(uint8_t *byte) {
         tickdelay(micros_to_ticks(DELAY));
     }
     return TIMEOUT_ERROR;
-}
-
-int (mouse_read_ack)(uint8_t *byte) {
-    int ret = 0;
-    uint8_t stat;
-    //for(int i = 0; i < KBC_NUM_TRIES; ++i){
-        if((ret = util_sys_inb(STATUS_REG, &stat))) return ret;
-        //if((stat&OUT_BUF_FUL) && (stat&AUX_MOUSE)) {
-            if(stat & (PARITY_ERROR | TIME_OUT_REC)) return OTHER_ERROR;
-            if((ret = util_sys_inb(OUTPUT_BUF, byte))) return ret;
-            else return SUCCESS;
-        //}
-        //tickdelay(micros_to_ticks(DELAY));
-    //}
-    //return TIMEOUT_ERROR;
 }
 
 int (mouse_poll_byte)(uint8_t *byte, uint16_t period) {
