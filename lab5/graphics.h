ce986ec556365a48a66f9381714240603600de87
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-11-11 18:48:13 +0000

    added files

---
+++ lab5/graphics.h
@@ -0,0 +1,13 @@
+#ifndef GRAPHICS_H_INCLUDED
+#define GRAPHICS_H_INCLUDED
+
+#include <stdint.h>
+
+/**
+ * @brief
+ * @param mode
+ * @return
+ */
+int (set_graphics_mode)(uint16_t mode);
+
+#endif /* end of include guard: GRAPHICS_H_INCLUDED */
