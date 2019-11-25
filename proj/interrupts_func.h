494b732117450653013bf1611ef91c4a02725077
up201806554 <up201806554@84c98234-7846-4868-9730-29c3f42930e9>
2019-11-25 19:22:05 +0000

    added files

---
+++ proj/interrupts_func.h
@@ -0,0 +1,26 @@
+#ifndef INTERRUPTS_FUNC_H_INCLUDED
+#define INTERRUPTS_FUNC_H_INCLUDED
+
+#include <stdint.h>
+
+/**
+ * @brief Subscribes all drivers used (timer->keyboard->mouse)
+ * @return ERROR_CODE code representing the result of the operation, SUCCESS code is returned if everything is OK
+ * @see {_ERRORS_H_::errors}
+ */
+int (subscribe_all)(void);
+
+/**
+ * @brief Unsubscribes all subscribed interrupts
+ * @return ERROR_CODE code representing the result of the operation, SUCCESS code is returned if everything is OK
+ * @see {_ERRORS_H_::errors}
+ */
+int (unsubscribe_all)(void);
+
+/**
+ * @brief Calls interrupt handler corresponding to the bit specified by the argument.
+ * @param handler Interrupt handler to call
+ */
+void interrupt_handler(uint8_t handler);
+
+#endif /* end of include guard: INTERRUPTS_FUNC_H_INCLUDED */
