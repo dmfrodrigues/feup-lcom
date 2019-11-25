d2012b7dfb7585d7ee4f3555658031bd0052ee74
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-11-25 18:19:02 +0000

    added macro

--- proj/proj_macros.h
+++ proj/proj_macros.h
@@ -2,20 +2,23 @@
 #define PROJ_MACROS_H_INCLUDED
 
 // WASD Movement Keys
-#define W_MAKE_CODE     0x11    /* @brief W Make Code */
-#define W_BREAK_CODE    0x91    /* @brief W Make Code */
-#define A_MAKE_CODE     0x1E    /* @brief A Make Code */
-#define A_BREAK_CODE    0x9E    /* @brief A Make Code */
-#define S_MAKE_CODE     0x1F    /* @brief S Make Code */
-#define S_BREAK_CODE    0x9F    /* @brief S Make Code */
-#define D_MAKE_CODE     0x20    /* @brief D Make Code */
-#define D_BREAK_CODE    0xA0    /* @brief D Make Code */
+#define W_MAKE_CODE     0x11    /** @brief W Make Code */
+#define W_BREAK_CODE    0x91    /** @brief W Make Code */
+#define A_MAKE_CODE     0x1E    /** @brief A Make Code */
+#define A_BREAK_CODE    0x9E    /** @brief A Make Code */
+#define S_MAKE_CODE     0x1F    /** @brief S Make Code */
+#define S_BREAK_CODE    0x9F    /** @brief S Make Code */
+#define D_MAKE_CODE     0x20    /** @brief D Make Code */
+#define D_BREAK_CODE    0xA0    /** @brief D Make Code */
 
 // Extra Keys
-#define ESC_MAKE_CODE   0x01    /* @brief ESC Make Code */
-#define ESC_BREAK_CODE  0x81    /* @brief ESC Break Code */
+#define ESC_MAKE_CODE   0x01    /** @brief ESC Make Code */
+#define ESC_BREAK_CODE  0x81    /** @brief ESC Break Code */
 
 // Refresh Rate
 #define REFRESH_RATE    60      /** @brief Screen refresh rate */
+
+//Graphics mode
+#define GRAPH_MODE      DIRECT_1280_1024_888 /** @brief Graphic mode used */
 
 #endif /* end of include guard: PROJ_MACROS_H_INCLUDED */
