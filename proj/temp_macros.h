8cdbff5d97d113a6dced19b2824fca56894f0cf4
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-11-25 17:05:57 +0000

    import proj

---
+++ proj/temp_macros.h
@@ -0,0 +1,18 @@
+#ifndef TEMP_MACROS_H_INCLUDED
+#define TEMP_MACROS_H_INCLUDED
+
+// WASD Movement Keys
+#define W_MAKE_CODE     0x11    /* @brief W Make Code */
+#define W_BREAK_CODE    0x91    /* @brief W Make Code */
+#define A_MAKE_CODE     0x1E    /* @brief A Make Code */
+#define A_BREAK_CODE    0x9E    /* @brief A Make Code */
+#define S_MAKE_CODE     0x1F    /* @brief S Make Code */
+#define S_BREAK_CODE    0x9F    /* @brief S Make Code */
+#define D_MAKE_CODE     0x20    /* @brief D Make Code */
+#define D_BREAK_CODE    0xA0    /* @brief D Make Code */
+
+// Extra Keys
+#define ESC_MAKE_CODE   0x01    /* @brief ESC Make Code */
+#define ESC_BREAK_CODE  0x81    /* @brief ESC Break Code */
+
+#endif /* end of include guard: TEMP_MACROS_H_INCLUDED */
