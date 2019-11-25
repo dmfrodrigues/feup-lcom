7816bb12d309b67ac37989feefc403580dae9007
up201806554 <up201806554@84c98234-7846-4868-9730-29c3f42930e9>
2019-11-25 19:19:58 +0000

    change on ihs

--- proj/keyboard.h
+++ proj/keyboard.h
@@ -5,6 +5,13 @@
 #ifndef KEYBOARD_H_INCLUDED
 #define KEYBOARD_H_INCLUDED
 
+/**
+ * @brief Subscribes Keyboard Interrupts and disables Minix Default IH
+ * @param interrupt_bit Bit of Interrupt Vector that will be set when Keyboard Interrupt is pending
+ * @param interrupt_id Keyboard Interrupt ID to specify the Keyboard Interrupt in other calls
+ * @return ERROR_CODE code representing the result of the operation, SUCCESS code is returned if everything is OK
+ * @see {_ERRORS_H_::errors}
+ */
 int (subscribe_kbc_interrupt)(uint8_t interrupt_bit, int *interrupt_id);
 
 uint8_t scancode[2];
