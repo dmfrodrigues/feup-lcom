0d863fe67f8904d950266a169ca9a85ed5800010
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-11-25 17:32:07 +0000

    changed things

--- proj/proj.c
+++ proj/proj.c
@@ -2,7 +2,9 @@
 #include <lcom/proj.h>
 #include <lcom/liblm.h>
 
+#include <stdbool.h>
 #include <stdint.h>
+
 
 int main(int argc, char* argv[]) {
 
@@ -17,4 +19,36 @@ int main(int argc, char* argv[]) {
     lcf_cleanup();
 
     return 0;
+}
+
+static int print_usage() {
+  printf("Usage: <mode - hex>\n");
+
+  return 1;
+}
+
+int(proj_main_loop)(int argc, char *argv[]) {
+    /// cycle
+    int good = 1;
+    while (good) {
+        /* Get a request message. */
+        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+            printf("driver_receive failed with %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE: /* hardware interrupt notification */
+                    if (msg.m_notify.interrupts & kbc_irq) { /* subscribed interrupt */
+                        kbc_ih();
+                        if (scancode[0] == ESC_BREAK_CODE) good = 0;
+                    }
+                    break;
+                default:
+                    break; /* no other notifications expected: do nothing */
+            }
+        } else { /* received standart message, not a notification */
+            /* no standart message expected: do nothing */
+        }
+    }
 }
