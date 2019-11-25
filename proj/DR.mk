8b2b0a04e98c9740b52a9684973137bf7b0152eb
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-11-25 19:36:57 +0000

    changed makefiles

--- proj/DR.mk
+++ proj/DR.mk
@@ -1,6 +1,6 @@
 PROG=proj
 
-SRCS= proj.c graphics.c sprite.c kbc.c keyboard.c mouse.c utils.c timer.c
+SRCS= proj.c graphics.c sprite.c kbc.c keyboard.c mouse.c utils.c timer.c interrupts_func.c proj_func.c
 
 CPPFLAGS += -pedantic -D __LCOM_OPTIMIZED_ -D DIOGO
 
