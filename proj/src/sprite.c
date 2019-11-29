cf3c8b5acc2da8145131706d1a815a8aa0459e92
up201806554 <up201806554@84c98234-7846-4868-9730-29c3f42930e9>
2019-11-29 15:36:23 +0000

    sprite cannot go off screen

--- proj/src/sprite.c
+++ proj/src/sprite.c
@@ -40,7 +40,10 @@ void (sprite_dtor)(sprite_t *p){
     free(p);
 }
 
-void (sprite_set_pos)   (sprite_t *p, int16_t x, int16_t y){ p->x = x; p->y = y; }
+void (sprite_set_pos)   (sprite_t *p, int16_t x, int16_t y) {
+    p->x = max(0, x); p->y = max(0, y);
+    p->x = min(p->x, graph_get_XRes() - 1); p->y = min(p->y, graph_get_YRes() - 1);
+}
 void (sprite_set_angle) (sprite_t *p, double angle){ p->theta = angle; }
 void (sprite_set_center)(sprite_t *p, int16_t u0, int16_t v0){ p->u0 = u0; p->v0 = v0; }
 
