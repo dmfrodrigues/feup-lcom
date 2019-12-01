29aec9edabd34516768cadb96c026ef75fe63a95
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-01 19:04:40 +0000

    forgot files

---
+++ proj/xpm/nothing.h
@@ -0,0 +1,11 @@
+#ifndef NOTHING_H_INCLUDED
+#define NOTHING_H_INCLUDED
+
+#include "nothing.xpm"
+#include "sprite.h"
+
+basic_sprite_t* get_nothing(void){
+    return basic_sprite_ctor((const char **)nothing_xpm, 34, 34);
+}
+
+#endif //NOTHING_H_INCLUDED
