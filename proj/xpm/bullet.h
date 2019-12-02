4963a682da8a93a52220a96da12090c0c4a631c5
Telmo <44447503+Telmooo@users.noreply.github.com>
2019-12-02 19:41:43 +0000

    map

--- proj/xpm/bullet.h
+++ proj/xpm/bullet.h
@@ -1,6 +1,11 @@
+#ifndef BULLET_H_INCLUDED
+#define BULLET_H_INCLUDED
+
 #include "bullet.xpm"
 #include "sprite.h"
 
 basic_sprite_t* get_bullet(void){
     return basic_sprite_ctor((const char **)bullet_xpm, 3, 7);
 }
+
+#endif /* end of include guard: BULLET_H_INCLUDED */
---
+++ proj/xpm/map.h
@@ -0,0 +1,11 @@
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+#include "map.xpm"
+#include "sprite.h"
+
+basic_sprite_t* get_map(void){
+    return basic_sprite_ctor((const char **)map, 639, 511);
+}
+
+#endif /* end of include guard: MAP_H_INCLUDED */
