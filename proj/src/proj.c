d6d933c923d832ea87d91c2270a85c2f2f7d639b
up201806554 <up201806554@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-02 19:10:40 +0000

    bullet things

--- proj/src/proj.c
+++ proj/src/proj.c
@@ -118,8 +118,8 @@ int(proj_main_loop)(int argc, char *argv[]) {
         gunner_t *shooter2 = gunner_ctor(bsp_shooter, bsp_nothing);
         gunner_set_pos(shooter2, -50, -50);
 
-        ent_t *bullet = ent_ctor(get_bullet(), bsp_nothing);
-        ent_set_pos(bullet, 400, 400);
+        bullet_t *bullet = bullet_ctor(get_bullet());
+        bullet_set_pos(bullet, 400, 400);
 
         graph_clear_screen();
         gunner_draw(shooter1);
@@ -156,8 +156,8 @@ int(proj_main_loop)(int argc, char *argv[]) {
                                 if (no_interrupts % refresh_count_value == 0) {
                                     update_movement(shooter1);
                                     update_scale();
-                                    ent_set_origin(gunner_get_x(shooter1)-ent_get_XLength()/2.0,
-                                                   gunner_get_y(shooter1)-ent_get_YLength()/2.0);
+                                    ent_set_origin(bullet_get_x(bullet)-ent_get_XLength()/2.0,
+                                                   bullet_get_y(bullet)-ent_get_YLength()/2.0);
 
                                     sprite_set_pos(sp_crosshair, get_mouse_X(), get_mouse_Y());
                                     double angle = get_mouse_angle(shooter1);
@@ -165,7 +165,7 @@ int(proj_main_loop)(int argc, char *argv[]) {
                                     graph_clear_screen();
                                     gunner_draw(shooter2);
                                     gunner_draw(shooter1);
-                                    ent_draw(bullet);
+                                    bullet_draw(bullet);
                                     sprite_draw(sp_crosshair);
                                     graph_draw();
                                 }
