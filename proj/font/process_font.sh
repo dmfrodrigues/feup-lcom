6a4a3ac0398416ffb53b31ef6f1348ae2a02c7b5
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-12-12 18:41:51 +0000

    changed stuff place

--- proj/font/process_font.sh
+++ proj/font/process_font.sh
@@ -2,9 +2,9 @@ echo Processing $1
 echo "    Splitting font"
 python3 split_font.py $1 $2 $3 $4 $5 $6
 echo "    Converting from png to xpm"
-./png_to_xpm.sh $1
+../helpers/png_to_xpm.sh $1
 echo "    Converting from xpm to xpm2"
-./xpm_to_xpm2.sh $1
+../helpers/xpm_to_xpm2.sh $1
 echo "    Cleaning up"
 rm -rf $1/png
 rm -rf $1/xpm
