ad432b3f4542348d0060f4ef892103a1ea3648e8
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-12 18:41:51 +0000

    changed stuff place

--- proj/maps/map1.h
+++ proj/maps/map1.h
@@ -5,8 +5,13 @@
 #include "map1_collide.xpm"
 #include "sprite.h"
 
+#include "xpm_utils.h"
+
 map_t* get_map1(void){
+    char **map1         = xpm_load_xpm2("maps/map1.xpm2");
+    char **map1_collide = xpm_load_xpm2("maps/map1_collide.xpm2");
     return map_ctor((const char **)map1_xpm, (const char **)map1_collide_xpm);
+    free(map1); free(map1_collide);
 }
 
 #endif /* end of include guard: MAP_H_INCLUDED */
