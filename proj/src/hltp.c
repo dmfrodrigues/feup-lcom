7082e7ff976562a13d76df226676913ac7889ec9
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-29 01:27:07 +0000

    added files

---
+++ proj/src/hltp.c
@@ -0,0 +1,11 @@
+#include <lcom/lcf.h>
+
+#include "hltp.h"
+
+#include "nctp.h"
+
+int hltp_send_string(int port, const char *p){
+    uint8_t* ptr[] = {p};
+    size_t    sz[] = {strlen(p)};
+    return nctp_send(port, 1, ptr, sz);
+}
