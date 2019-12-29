71b93e35fee0212231d5e94e67b230350156eb33
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-12-29 01:27:07 +0000

    added files

---
+++ proj/include/hltp.h
@@ -0,0 +1,6 @@
+#ifndef HLTP_H_INCLUDED
+#define HLTP_H_INCLUDED
+
+int hltp_send_string(int port, const char *p);
+
+#endif //HLTP_H_INCLUDED
---
+++ proj/include/nctp.h
@@ -0,0 +1,16 @@
+#ifndef NCTP_H_INCLUDED
+#define NCTP_H_INCLUDED
+
+#include "uart.h"
+
+int nctp_get_char_poll   (int base_addr, uint8_t *p);
+int nctp_send_char_poll  (int base_addr, uint8_t  c);
+
+/**
+ * @brief Send stuff to COM #port.
+ * @param   port    Port to send message to
+ * @param   num     Number of pairs of address/numbytes to send
+ */
+int nctp_send(int port, size_t num, uint8_t* ptr[], size_t sz[]);
+
+#endif //NCTP_H_INCLUDED
