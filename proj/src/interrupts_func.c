a2b8196674bd1d409a00d955250124e5dd6cfede
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2019-12-30 03:02:08 +0000

    changed bit rate to 38400 bps

--- proj/src/interrupts_func.c
+++ proj/src/interrupts_func.c
@@ -101,6 +101,10 @@ int (subscribe_all)(void) {
 
     /// UART interrupt handling
     uart_id = 0;
+    uart_set_bits_per_character(COM1_ADDR, 8);
+    uart_set_stop_bits         (COM1_ADDR, 2);
+    uart_set_parity            (COM1_ADDR, uart_parity_even);
+    uart_set_bit_rate          (COM1_ADDR, 38400);
     uart_enable_int_rx (COM1_ADDR);
     uart_disable_int_tx(COM1_ADDR);
     if(subscribe_uart_interrupt(COM1_IRQ, &uart_id)) {
