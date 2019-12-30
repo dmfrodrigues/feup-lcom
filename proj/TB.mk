700b24aad3bcb5ca238f6065ede0bbb633b65f2e
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-30 23:58:19 +0000

    changed more stuff

--- proj/TB.mk
+++ proj/TB.mk
@@ -1,10 +1,19 @@
 PROG=proj
 
 .PATH: ${.CURDIR}/src
+.PATH: ${.CURDIR}/libs/graph/src
+.PATH: ${.CURDIR}/libs/kbc/src
+.PATH: ${.CURDIR}/libs/rtc/src
+.PATH: ${.CURDIR}/libs/timer/src
+.PATH: ${.CURDIR}/libs/uart/src
+.PATH: ${.CURDIR}/libs/classes/src
+.PATH: ${.CURDIR}/libs/utils/src
 
-SRCS= proj.c list.c queue.c graph.c kbc.c keyboard.c mouse.c utils.c timer.c interrupts_func.c proj_func.c fast_math.c rectangle.c font.c xpm_utils.c ent.c rtc.c uart.c hltp.c makecode_map.c
+SRCS= proj.c list.c queue.c graph.c kbc.c keyboard.c mouse.c utils.c timer.c interrupts_func.c proj_func.c fast_math.c rectangle.c font.c xpm_utils.c ent.c rtc.c uart.c makecode_map.c
+IPATHS=-I./include -I./libs/graph/include -I./libs/kbc/include -I./libs/rtc/include -I./libs/timer/include -I./libs/uart/include -I./libs/classes/include -I./libs/utils/include -I./maps -I./media/xpm
 
-CPPFLAGS += -pedantic -I./include -I./maps -I./media/xpm -D LCOM_MACRO -D TELMO #-D __LCOM_OPTIMIZED_
+CPPFLAGS += -pedantic ${IPATHS} -D LCOM_MACRO #-D __LCOM_OPTIMIZED_
+CPPFLAGS += -D TELMO
 
 DPADD += ${LIBLCF}
 LDADD += -llcf
