0cc2adf36a4193ef6f5890d4f99d93988d9ef164
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2019-12-30 22:39:22 +0000

    removing stuff

--- proj/include/nctp.h
+++ proj/include/nctp.h
@@ -1,14 +1,4 @@
 #ifndef NCTP_H_INCLUDED
 #define NCTP_H_INCLUDED
 
-#include "uart.h"
-
-int nctp_init(void);
-int nctp_free(void);
-
-int nctp_send(size_t num, uint8_t* ptr[], size_t sz[]);
-
-int nctp_ih_err;
-void nctp_ih(void);
-
 #endif //NCTP_H_INCLUDED
