9ebd4670e3083c0c0e30945910e2e6b83524ce71
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2020-01-03 22:02:59 +0000

    ommited outputs and traces

--- proj/src/proj.c
+++ proj/src/proj.c
@@ -38,9 +38,9 @@ int main(int argc, char* argv[]) {
 
     lcf_set_language("EN-US");
 
-    lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
+    //lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
 
-    lcf_log_output("/home/lcom/labs/proj/output.txt");
+    //lcf_log_output("/home/lcom/labs/proj/output.txt");
 
     if (lcf_start(argc, argv)) return 1;
 
