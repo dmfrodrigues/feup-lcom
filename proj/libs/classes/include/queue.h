16c963e9c23fc3ed02da47917393acc161dc4e9d
Diogo Rodrigues <43684166+dmfrodrigues@users.noreply.github.com>
2020-01-04 02:53:37 +0000

    documented list and queue

--- proj/libs/classes/include/queue.h
+++ proj/libs/classes/include/queue.h
@@ -2,16 +2,56 @@
 #define QUEUE_H_INCLUDED
 
 struct queue;
+/**
+ * @brief Queue.
+ *
+ * Can be used like a C++ std::queue.
+ * A queue_t is an interface for a list_t that simulates a FIFO.
+ */
 typedef struct queue queue_t;
 
-const size_t queue_max_size;
-
+/**
+ * @brief Construct empty queue.
+ * @return  Pointer to created queue
+ */
 queue_t* (queue_ctor )(void);
+/**
+ * @brief Destruct queue.
+ *
+ * A queue can only be destroyed once it is empty.
+ * This is because a queue stores void*, whose memory most likely need to be free'd.
+ * @param   q   Pointer to queue to be destroyed
+ * @return      SUCCESS if the destruction was successful, other value otherwise.
+ */
 int      (queue_dtor )(queue_t *q);
+/**
+ * @brief Get size of the queue.
+ * @param   q   Pointer to queue
+ * @return      Size of the queue
+ */
 size_t   (queue_size )(const queue_t *q);
+/**
+ * @brief Know if queue is empty or not.
+ * @param   q   Pointer to queue
+ * @return      true if the queue is empty (size is zero), false otherwise
+ */
 int      (queue_empty)(const queue_t *q);
+/**
+ * @brief Insert new value in the queue.
+ * @param   q   Pointer to queue
+ * @param   val Value to be inserted
+ */
 void     (queue_push )(queue_t *q, void *val);
+/**
+ * @brief Get next element in the queue.
+ * @param   q   Pointer to queue
+ * @return      Value at the beginning of the queue
+ */
 void*    (queue_top  )(const queue_t *q);
+/**
+ * @brief Erase next element of the queue.
+ * @param   q   Pointer to queue
+ */
 void     (queue_pop  )(queue_t *q);
 
 #endif //QUEUE_H_INCLUDED
--- proj/tasks.md
+++ proj/tasks.md
@@ -3,12 +3,16 @@
 ### Most priority
 - [ ] Implement multiplayer 1v1 game and win condition to reach N kills.
 - [ ] Leaderboard of fastest win, or any achievement (to make use of RTC).
+- [ ] Documentation
+- [ ] Report
 
 ### Mid priority
-- [ ] Implement path finding for melee AI.
-- [ ] Implement zombie mode (single-player or co-op).
+- [x] Implement path finding for melee AI.
+- [x] Implement zombie mode (single-player)
+- [ ] Implement zombie mode (co-op)
 - [ ] Implement algorithm to decide when gunner AI needs to shoot.
-- [ ] Implement campaign mode (single-player or co-op).
+- [ ] Implement campaign mode (single-player)
+- [ ] Implement campaign mode (co-op).
 - [ ] Visibility algorithm
 
 ### Low Priority
