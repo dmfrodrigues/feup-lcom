0f2094347629e8ed83c15f80bcffb07ac2047624
up201806429 <up201806429@84c98234-7846-4868-9730-29c3f42930e9>
2020-01-05 21:02:55 +0000

    corrected minor error

--- proj/maps/map1.h
+++ proj/maps/map1.h
@@ -8,10 +8,10 @@
 #include "xpm_utils.h"
 
 static map_t* get_map1(void){
-    char **map1         = xpm_load_xpm2("maps/map1.xpm2");
-    char **map1_collide = xpm_load_xpm2("maps/map1_collide.xpm2");
-    return map_ctor((const char *const *)map1_xpm, (const char *const *)map1_collide_xpm);
-    free(map1); free(map1_collide);
+    char **ret         = xpm_load_xpm2("maps/map1.xpm2");
+    char **ret_collide = xpm_load_xpm2("maps/map1_collide.xpm2");
+    return map_ctor((const char *const *)ret, (const char *const *)ret_collide);
+    free(ret); free(ret_collide);
 }
 
 #endif /* end of include guard: MAP_H_INCLUDED */
